09eaf29ffafc7f67e7b3ecaf33862bd2477ddb00
Confidence <connymtimba@gmail.com>
2023-07-13 08:52:24 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-     <Weather />
+      <Weather />
+      <footer>
+        This project was coded by {""}
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          ConnieMtimba
+        </a>{" "}
+        and is{""}
+        {""}{" "}
+        <a
+          href="https://github.com/ConnieMtimba/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,41 +5,50 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="container">
-        <form>
-          <div className="row">
-            <div className="col-8">
-              <input
-                type="search"
-                placeholder="Enter City..."
-                className="form-control"
-              />
-            </div>
-            <div className="col-4">
-              <input type="submit" value="Search" className="btn btn-primary" />
-            </div>
-          </div>
-        </form>
-        <h1>Harare</h1>
-        <ul>
-          <li>Tuesday 15:30</li>
-          <li>Partly Cloudy</li>
-        </ul>
+      <form>
         <div className="row">
-          <div className="col-6">
+          <div className="col-8">
+            <input
+              type="search"
+              placeholder="Enter City..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-4">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Harare</h1>
+      <ul>
+        <li>Tuesday 15:30</li>
+        <li>Partly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="Partly Cloudy"
+              className="float-left"
             />
-            8°C
+            <div className="float-left">
+              <span className="temperature">8</span>
+              <span className="units">°C</span>
+            </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation:0%</li>
-              <li>Humidity:73%</li>
-              <li>Wind:26km/h</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:0%</li>
+            <li>Humidity:73%</li>
+            <li>Wind:26km/h</li>
+          </ul>
         </div>
       </div>
     </div>
