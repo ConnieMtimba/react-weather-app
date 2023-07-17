661f15474c24d67ca56884139d7d2e432c4848b1
Confidence <connymtimba@gmail.com>
2023-07-17 07:23:34 +0200

    Added animated weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: response.data.weather[0].icon,
     });
   }
 
